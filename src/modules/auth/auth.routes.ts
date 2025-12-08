01c5506caf09721d5f9e4b30d23d00f1b6e88a5c
coderzhaxor <ajanuantara@gmail.com>
2025-12-08 14:33:20 +0700

    Remove auth middleware from logout route in auth router

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,15 +1,11 @@
 import { Hono } from "hono";
 import { login, logout, refresh, register } from "./auth.controller";
-import { authMiddleware } from "../../middlewares/auth.middleware";
 
 const authRouter = new Hono()
 
-// Public Routes
 authRouter.post("/register", register)
 authRouter.post("/login", login)
 authRouter.post("/refresh", refresh)
-
-// Protected Route
-authRouter.post("/logout", authMiddleware, logout)
+authRouter.post("/logout", logout)
 
 export default authRouter;
